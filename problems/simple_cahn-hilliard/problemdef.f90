62f43972801e16729dc5f415f5a8c550340ebf3c
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2014-02-23 13:10:40 +0100

    Clean up and fix sample: problemdef.f90

--- problems/simple_cahn-hilliard/problemdef.f90
+++ problems/simple_cahn-hilliard/problemdef.f90
@@ -1,7 +1,6 @@
-MODULE ProblemDef
-USE NodeInfoDef, ONLY: r8
-IMPLICIT NONE
-!
-REAL(KIND=r8), PUBLIC:: alpha, eps, eps2
-!
-END MODULE ProblemDef
+module ProblemDef
+  implicit none
+
+  real, public:: alpha, eps, eps2
+
+end module ProblemDef
